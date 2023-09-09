31a230dffd787aa1b78b6e78384eb50c0ba3de3e
Gleb <stepanov-gleb2003@mail.ru>
2023-09-09 11:25:09 +0300

    Сделал стили для мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,22 @@
+const mainNavButton = document.querySelector('.main-nav__toggle');
+const mainNav = document.querySelector('.main-nav');
+const body = document.body;
+
+mainNavButton.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+
+    body.classList.add('page__body--darkness');
+  } else {
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
+
+    body.classList.remove('page__body--darkness');
+  }
+});
+
+
 const swiper = new Swiper('.swiper', {
   loop: true,
   watchSlidesProgress: true,
